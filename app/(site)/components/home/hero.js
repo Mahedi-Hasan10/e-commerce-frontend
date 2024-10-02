4768cac52f7b6d7ba718e14bd25a54049f2503ca
Anikcse18 <istiakjavedanik@gmail.com>
2024-10-02 16:18:31 +0600

    add footer and bottom part of the hero section

--- app/(site)/components/home/hero.js
+++ app/(site)/components/home/hero.js
@@ -4,93 +4,61 @@ import Button from "../common/button";
 import { FaArrowRightLong } from "react-icons/fa6";
 
 const Hero = () => {
+  const items = [
+    {
+      image: "/logo_box.png",
+      title: "Fasted Delivery",
+      details: "Delivery in 24/H",
+    },
+
+    {
+      image: "/logo_trophy.png",
+      title: "24 Hours Return",
+      details: "100% money-back guarantee",
+    },
+
+    {
+      image: "/logo_card.png",
+      title: "Secure Payment",
+      details: "Your money is safe",
+    },
+    {
+      image: "/logo_headphone.png",
+      title: "Support 24/7",
+      details: "Live contact/message",
+    },
+  ];
+
   return (
-    <div className="py-6 container public-sans">
-      <div className="flex items-center gap-6">
-        <div className="flex items-center justify-center gap-6 h-[520px] w-[65%] bg-[#f2f4f5]">
-          <div className="max-w-[356px]">
-            <p className="text-blue-700 font-semibold text-lg mb-3">
-              --- THE BEST PLACE TO PLAY
-            </p>
-            <h2 className="xl-text-[60px] md:text-[40px] text-[26px] font-bold mb-4">
-              Xbox Consoles
-            </h2>
-            <p className="text-gray-700 md:text-lg text-base mb-5">
-              Save up to 50% on select Xbox games. Get 3 months of PC Game Pass
-              for $2 USD.
-            </p>
-            <Button
-              name={
-                <span className="flex gap-2 items-center ">
-                  Shop Now <FaArrowRightLong />
-                </span>
-              }
-              className="bg-[#fa8232] cursor-pointer hover:bg-yellow-600 text-white w-fit"
-            />
-          </div>
-          <div className="relative pr-6">
-            <Image
-              src="/hero1.png"
-              alt="hero"
-              height={1000}
-              width={1000}
-              className="w-[368px] h-[408px]"
-            />
-            <div className="h-[100px] w-[100px] flex items-center justify-center text-2xl font-bold text-white rounded-full bg-[#2da3f5] absolute top-0 right-[20px]">
-              $299
+    <div className="py-6 container public-sans ">
+      <div className="grid sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-4  gap-[41px] mt-[40px] border-2 rounded-[6px] p-[20px]">
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className={`flex gap-2 items-center  ${
+              index === items.length - 1
+                ? "md:border-none"
+                : index === 1
+                ? "lg:border-r-2"
+                : "md:border-r-2 "
+            } `}
+          >
+            <div className="flex gap-2 items-center">
+              <div className="">
+                <Image
+                  src={item.image}
+                  alt={item.title} // Use the title for alt text
+                  width={50}
+                  height={50}
+                />
+              </div>
+              <div>
+                <p className="text-sm uppercase">{item.title}</p>
+                <p className="text-[#5F6C72] text-sm">{item.details}</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="w-[30%] flex flex-col gap-6">
-          <div className="bg-[#141414] rounded-[2px] flex items-end gap-5 relative">
-            <div className="py-[30px] px-10">
-              <h3 className="text-lg text-yellow-500 mb-1">Summer Sale</h3>
-              <h3 className="text-xl font-bold mb-2 text-white">
-                Xiaomi FlipBuds Pro
-              </h3>
-              <h3 className="text-lg font-bold text-blue-500 mb-2">$299</h3>
-              <Button
-                name={
-                  <span className="flex gap-2 items-center ">
-                    Shop Now <FaArrowRightLong />
-                  </span>
-                }
-                className="bg-[#fa8232] cursor-pointer hover:bg-yellow-600 text-white w-fit"
-              />
-            </div>
-            <Image
-              src="/hero2.png"
-              alt="hero3"
-              height={500}
-              width={500}
-              className="h-[160px] w-[160px]"
-            />
-            <div className="py-2 px-4 text-lg font-semibold text-white rounded-[5px] bg-[#2da3f5] absolute top-[8%] right-[20px]">
-              29% Off
-            </div>
-          </div>
-          <div className="bg-[#f2f4f5] rounded-[2px] py-11 px-8 flex items-center gap-5">
-            <Image
-              src="/hero3.png"
-              alt="hero3"
-              height={500}
-              width={500}
-              className="h-[160px] w-[160px]"
-            />
-            <div>
-              <h3 className="text-2xl font-bold mb-3">Xiaomi FlipBuds Pro</h3>
-              <h3 className="text-xl font-bold text-blue-500 mb-5">$299</h3>
-              <Button
-                name={
-                  <span className="flex gap-2 items-center ">
-                    Shop Now <FaArrowRightLong />
-                  </span>
-                }
-                className="bg-[#fa8232] cursor-pointer hover:bg-yellow-600 text-white w-fit"
-              />
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- app/(site)/components/layout/footer.js
+++ app/(site)/components/layout/footer.js
@@ -21,108 +21,128 @@ const Footer = () => {
     "Customer Help",
     "About Us",
   ];
+
+  const tags = [
+    "Game",
+    "iPhone",
+    "TV",
+    "Asus Laptops",
+    "Macbook",
+    "SSD",
+    "Graphics Card",
+    "Power Bank",
+    "Smart TV",
+    "Speaker",
+    "Tablet",
+    "Microwave",
+    "Samsung",
+  ];
+
   // className="flex flex-col lg:flex-row  lg:justify-between  py-16"
   return (
-    <section className="text-white public-sans container ">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5  gap-4 py-16">
-        <div className="">
-          <div className="flex items-center gap-2 text-white font-bold text-3xl">
-            <FaBagShopping className="text-yellow-500" />
-            <span>MStore</span>
+    <footer className="text-white public-sans">
+      <section className=" container ">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-5  gap-4 py-16">
+          <div className="">
+            <div className="flex items-center gap-2 text-white font-bold text-3xl">
+              <FaBagShopping className="text-yellow-500" />
+              <span>MStore</span>
+            </div>
+            <div className="space-y-2 mt-4 public-sans">
+              <p className="text-[#77878F]">Customer Support</p>
+              <h3>{customerSupport.phone}</h3>
+              <h3 className="max-w-[248px] text-[#77878F]">
+                {customerSupport.address}
+              </h3>
+              <h3 className="">{customerSupport.email}</h3>
+            </div>
           </div>
-          <div className="space-y-2 mt-4 public-sans">
-            <p className="text-[#77878F]">Customer Support</p>
-            <h3>{customerSupport.phone}</h3>
-            <h3 className="max-w-[248px] text-[#77878F]">
-              {customerSupport.address}
-            </h3>
-            <h3 className="">{customerSupport.email}</h3>
-          </div>
-        </div>
-        <div>
-          <h1 className="uppercase">Top Category</h1>
-          <div className="mt-4">
-            <ul className="text-[#77878F] space-y-3">
-              {categories.map((product, idx) => (
-                <li key={idx}>{product}</li>
-              ))}
-            </ul>
-
-            <div>
-              <div className="flex items-center my-3">
-                <div className="border-t-2 border-yellow-400 h-0 w-[26px] mr-2"></div>
-                <p>Accessories</p>
-              </div>
+          <div>
+            <h1 className="uppercase">Top Category</h1>
+            <div className="mt-4">
               <ul className="text-[#77878F] space-y-3">
-                {accessories.map((accessorie, idx) => (
-                  <li key={idx}>{accessorie}</li>
+                {categories.map((product, idx) => (
+                  <li key={idx}>{product}</li>
                 ))}
               </ul>
-              <Link href={"/"}>
-                <div className="flex items-center gap-3 text-yellow-500 py-2 text-xl">
-                  <h1>Browse All Product</h1>
-                  <FaArrowRightLong />
-                </div>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div>
-          <h1 className="uppercase">Quick Links</h1>
-          <ul className="text-[#77878F] space-y-3 mt-4">
-            {quicklinks.map((link, idx) => (
-              <li key={idx}>
-                <Link href={"/"}>{link}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <h1 className="uppercase">Download App</h1>
-          <div className="mt-4 space-y-5">
-            <div className="flex gap-4 max-w-[176px] max-h-[70px] p-4 bg-[#303639] rounded-md">
-              <div className="text-4xl">
-                <IoLogoGooglePlaystore />
-              </div>
-              <div>
-                <p className="text-xs">Get it on</p>
-                <h1 className="text-base">Google Play</h1>
-              </div>
-            </div>
 
-            <div className="flex gap-4 p-4 max-w-[176px] max-h-[70px] bg-[#303639] rounded-md">
-              <div className="text-4xl">
-                <FaApple />
-              </div>
               <div>
-                <p className="text-xs">Get it on</p>
-                <h1 className="text-base">Apple Store</h1>
+                <div className="flex items-center my-3">
+                  <div className="border-t-2 border-yellow-400 h-0 w-[26px] mr-2"></div>
+                  <p>Accessories</p>
+                </div>
+                <ul className="text-[#77878F] space-y-3">
+                  {accessories.map((accessorie, idx) => (
+                    <li key={idx}>{accessorie}</li>
+                  ))}
+                </ul>
+                <Link href={"/"}>
+                  <div className="flex items-center gap-3 text-yellow-500 py-2 text-lg">
+                    <h1>Browse All Product</h1>
+                    <FaArrowRightLong />
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h1 className="uppercase">Quick Links</h1>
+            <ul className="text-[#77878F] space-y-3 mt-4">
+              {quicklinks.map((link, idx) => (
+                <li key={idx}>
+                  <Link href={"/"}>{link}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h1 className="uppercase">Download App</h1>
+            <div className="mt-4 space-y-5">
+              <div className="flex gap-1 md:gap-4 max-w-[176px] max-h-[70px] p-2 md:p-4 bg-[#303639] rounded-md">
+                <div className="text-4xl">
+                  <IoLogoGooglePlaystore />
+                </div>
+                <div className="whitespace-nowrap">
+                  <p className="text-xs">Get it on</p>
+                  <h1 className="text-sm font-bold lg:text-base">
+                    Google Play
+                  </h1>
+                </div>
+              </div>
+              {/* px-[2px] py-2 md:p-2 */}
+              <div className="flex gap-1 md:gap-4 max-w-[176px] max-h-[70px] p-2 md:p-4 bg-[#303639] rounded-md">
+                <div className="text-4xl">
+                  <FaApple />
+                </div>
+                <div>
+                  <p className="text-xs">Get it on</p>
+                  <h1 className="text-sm font-bold lg:text-base">
+                    Apple Store
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h1 className="uppercase">Popular Tag</h1>
+            <div className="mt-4 max-w-[312px]">
+              {/* className="flex flex-wrap gap-4" */}
+
+              <div className="flex flex-wrap gap-4">
+                {tags.map((tag) => (
+                  <button className="category" key={tag.idx}>
+                    {tag}
+                  </button>
+                ))}
               </div>
             </div>
           </div>
         </div>
-        <div>
-          <h1 className="uppercase">Popular Tag</h1>
-          <div className="mt-4 max-w-[312px]">
-            <div className="flex flex-wrap gap-4">
-              <button className="category">Game</button>
-              <button className="category">iPhone</button>
-              <button className="category">TV</button>
-              <button className="category">Asus Laptops</button>
-              <button className="category">Macbook</button>
-              <button className="category">SSD</button>
-              <button className="category">Graphics Card</button>
-              <button className="category">Power Bank</button>
-              <button className="category">Smart TV</button>
-              <button className="category">Speaker</button>
-              <button className="category">Tablet</button>
-              <button className="category">Microwave</button>
-              <button className="category">Samsung</button>
-            </div>
-          </div>
-        </div>
+      </section>
+      <div className="border-t-2 border-[#303639] text-center">
+        <h1 className="py-4">Copyright by @Istiak</h1>
       </div>
-    </section>
+    </footer>
   );
 };
 
